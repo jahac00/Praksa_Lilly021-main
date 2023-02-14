1f8dca1bdcfaabd1b19085943b2c23c57ea97524
Andrija17 <andrija.d.stanisic@gmail.com>
2023-02-14 13:15:43 +0100

    Update:SeparateMain

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
+import { BrowserRouter } from "react-router-dom";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
       <React.StrictMode>
-        <Main></Main>
-        <Footer></Footer>
+        <BrowserRouter>
+          <NavBar />
+          <Main />
+          <Footer />
+        </BrowserRouter>
       </React.StrictMode>
     </>
   );
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Categories from "../pages/Categories";
 import Glasses from "../pages/Glasses";
@@ -6,22 +6,18 @@ import Ingridients from "../pages/Ingridients";
 import BartenderBeginner from "../pages/BartenderBeginner";
 import BartenderVeteran from "../pages/BartenderVeteran";
 import CoctailDetails from "../pages/CoctailDetails";
-import NavBar from "./NavBar";
 
 function Main() {
   return (
-    <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/categories" element={<Categories />} />
-        <Route path="/glasses" element={<Glasses />} />
-        <Route path="/ingridients" element={<Ingridients />} />
-        <Route path="/bartender-beginner" element={<BartenderBeginner />} />
-        <Route path="/bartender-veteran" element={<BartenderVeteran />} />
-        <Route path="/coctail/:coctailId" element={<CoctailDetails />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/categories" element={<Categories />} />
+      <Route path="/glasses" element={<Glasses />} />
+      <Route path="/ingridients" element={<Ingridients />} />
+      <Route path="/bartender-beginner" element={<BartenderBeginner />} />
+      <Route path="/bartender-veteran" element={<BartenderVeteran />} />
+      <Route path="/coctail/:coctailId" element={<CoctailDetails />} />
+    </Routes>
   );
 }
 
